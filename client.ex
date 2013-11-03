b56fc556007381e23e1257df3663e740257455bf
David Baumgartner <_davidbaumgartner@bluewin.ch>
2013-11-03 21:59:25 +0100

    Removing local debug tool

--- client.ex
+++ client.ex
@@ -1,10 +1,5 @@
 defmodule Client do
 
-	# debug...
-	def strm() do
-		start_client({10,0,0,3},8491)
-	end
-
 	def start_client() do
 		dest_ip = list_to_tuple(Enum.map((String.split (IO.gets "IP to connect to ? "), "."), (fn (s) -> case String.to_integer s do { n, _ } -> n end end)))
 		{ dest_port, _ } = String.to_integer (IO.gets "Which port ? ")
@@ -19,9 +14,9 @@ defmodule Client do
 	end
 
 	defp loop(socket, ip, port) do 
-		ask = IO.gets "load> "
+		ask = String.strip (to_string (IO.gets "load> "))
 		case ask do
-			"end\n" ->
+			"end" ->
 				send_msg socket, ip, port, "end"
 			_ ->
 				send_msg socket, ip, port, ("{\"cmd\":\"load\",\"res\":\""<>ask<>"\"}")
